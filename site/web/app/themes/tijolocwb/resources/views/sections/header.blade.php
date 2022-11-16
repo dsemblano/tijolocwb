ac16133872086b7f4d14b7c4477ad5a72c2f62c6
Daniel Semblano <dsemblano@gmail.com>
2022-11-16 12:12:42 -0300

    H1 duplicity on pages removed

--- site/web/app/themes/tijolocwb/resources/views/sections/header.blade.php
+++ site/web/app/themes/tijolocwb/resources/views/sections/header.blade.php
@@ -1,12 +1,10 @@
 <header class="banner bg-tijolo">
   <div class="container flex flex-row-reverse xl:flex-row">
     @if (! is_front_page())
-    <h1 class="z-50">
-      <a class="brand" href="{{ home_url('/') }}">
-        {{-- {{ get_bloginfo('name', 'display') }} --}}
-        <img src="@asset('images/tijolo_logo.png')" width="61" height="43" alt="Tijolo Comidas e Vinhos" class="w-16 py-2" />
-      </a>
-    </h1>
+    <a class="brand" href="{{ home_url('/') }}">
+      {{-- {{ get_bloginfo('name', 'display') }} --}}
+      <img src="@asset('images/tijolo_logo.png')" width="61" height="43" alt="Tijolo Comidas e Vinhos" class="w-16 py-2" />
+    </a>
     @endif
     <nav class="nav-primary flex flex-row justify-evenly items-center w-full py-2" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
       @if (has_nav_menu('primary_navigation'))
