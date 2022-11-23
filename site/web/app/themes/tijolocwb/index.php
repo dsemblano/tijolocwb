a084fe9f5dbdb28cb0adebeee20bca9cfabd825c
Daniel Semblano <dsemblano@gmail.com>
2022-11-23 19:22:33 -0300

    Removed Clarity code and integreated to Google Tag Management

--- site/web/app/themes/tijolocwb/index.php
+++ site/web/app/themes/tijolocwb/index.php
@@ -5,7 +5,6 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
-  <?php echo \Roots\view('layouts/head/clarity')->render(); ?>
   <?php echo \Roots\view('layouts/head/favicon')->render(); ?>
   <?php echo \Roots\view('layouts/head/gconsole')->render(); ?>
   <?php echo \Roots\view('layouts/head/gtaghead')->render(); ?>
