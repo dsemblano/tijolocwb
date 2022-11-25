a07c596ddfe9ec40458b06022e036a04c86cd981
Daniel Semblano <dsemblano@gmail.com>
2022-11-24 21:04:39 -0300

    Google Maps iframe and h2 fixed

--- site/web/app/themes/tijolocwb/resources/views/partials/snippets/gmaps.blade.php
+++ site/web/app/themes/tijolocwb/resources/views/partials/snippets/gmaps.blade.php
@@ -5,5 +5,5 @@
   src="https://www.google.com/maps/embed//v1/place?key=AIzaSyA3o4YqY9yNd-SF0Funxb0jrsvjiu81_uY&location=-25.428062231132124,-49.27023167379688"
   allowfullscreen>
 </iframe> --}}
-<iframe class="w-full h-96 lg:w-3/5" style="border:0" loading="lazy" allowfullscreen
+<iframe title="Google Maps para endereço do Restaurante Tijolo" class="w-full h-96 lg:w-3/5" style="border:0" allowfullscreen
 src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJw85WChPk3JQRZetH0AALMO4&key=AIzaSyA3o4YqY9yNd-SF0Funxb0jrsvjiu81_uY"></iframe>
