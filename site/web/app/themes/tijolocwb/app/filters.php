532f902e55ce1e75c230f32eba44f5b7bdec56ad
Daniel Semblano <dsemblano@gmail.com>
2022-11-25 16:14:49 -0300

    js files defer

--- site/web/app/themes/tijolocwb/app/filters.php
+++ site/web/app/themes/tijolocwb/app/filters.php
@@ -51,3 +51,18 @@ add_filter('the_generator', 'remove_wp_version_rss');
 //     }
 //     return $classes;
 // });
+
+add_filter('script_loader_tag', function ($url) {
+    if ( is_user_logged_in() ) return $url; //don't break WP Admin
+    if ( FALSE === strpos( $url, '.js' ) ) return $url;
+    if ( strpos( $url, 'jquery.js' ) ) return $url;
+    return str_replace( ' src', ' defer src', $url );
+});
+
+// function defer_parsing_of_js( $url ) {
+//     if ( is_user_logged_in() ) return $url; //don't break WP Admin
+//     if ( FALSE === strpos( $url, '.js' ) ) return $url;
+//     if ( strpos( $url, 'jquery.js' ) ) return $url;
+//     return str_replace( ' src', ' defer src', $url );
+// }
+// add_filter( 'script_loader_tag', 'defer_parsing_of_js', 10 );
