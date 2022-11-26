b5c46ec06a73f4782864a54df6f5f06187aacfbd
Daniel Semblano <dsemblano@gmail.com>
2022-11-26 15:44:51 -0300

    preload custom fonts

--- site/web/app/themes/tijolocwb/resources/views/layouts/head/preload.blade.php
+++ site/web/app/themes/tijolocwb/resources/views/layouts/head/preload.blade.php
@@ -1,1 +1,3 @@
 <link rel="preload" as="image" href="@asset('images/logotijolo.png')">
+<link rel="preload" href="@asset('fonts/Acumin/acumin_pro_extracondensed-webfont.woff2')" as="font" type="font/woff2" crossorigin>
+<link rel="preload" href="@asset('fonts/Cuprum/Cuprum-Regular-webfont.woff2')" as="font" type="font/woff2" crossorigin>
