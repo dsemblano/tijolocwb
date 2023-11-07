d6ea3c706669a590d78d2576b355472ea26dcde5
Daniel Semblano <dsemblano@gmail.com>
2023-11-07 18:09:38 -0300

    home improvements

--- site/web/app/themes/tijolocwb/resources/views/partials/home/home3.blade.php
+++ site/web/app/themes/tijolocwb/resources/views/partials/home/home3.blade.php
@@ -1,9 +1,7 @@
 <section id="tela3" class="home-section h-screen bg-tijoloyellow">
-  <div class="container flex flex-col justify-center items-center h-full">
-    {{-- {!! do_shortcode('[wpgmza id="1"]') !!} --}}
-    {{-- {!! do_shortcode('[rank_math_contact_info class="w-full lg:w-3/5" show="map"]') !!} --}}
+  <div class="container flex flex-col justify-center items-center">
     @include('partials/snippets.gmaps')
-    <h2 class="text-lg xl:text-2xl text-gray-800 top-10">Rua São Francisco 179 loja 3,<br />Curitiba PR CEP 80020-190</h2>
-    <br>
+    {{-- <h2 class="text-lg xl:text-2xl text-gray-800 top-10">Rua São Francisco 179 loja 3,<br />Curitiba PR CEP 80020-190</h2>
+    <br> --}}
   </div>
 </section>
