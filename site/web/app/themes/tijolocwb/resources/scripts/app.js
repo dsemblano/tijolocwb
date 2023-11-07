4713d7c82061b0b301c95da6cc353f59f9e5e9a2
Daniel Semblano <dsemblano@gmail.com>
2023-11-07 17:37:19 -0300

    New typography for titles on menu page

--- site/web/app/themes/tijolocwb/resources/scripts/app.js
+++ site/web/app/themes/tijolocwb/resources/scripts/app.js
@@ -1,5 +1,39 @@
 import {domReady} from '@roots/sage/client';
 import 'flowbite/dist/flowbite.js';
+import Swiper from 'swiper/bundle';
+
+
+// const swiper = new Swiper(".mySwiper", {
+//   effect: "coverflow",
+//   grabCursor: true,
+//   centeredSlides: true,
+//   loop: true,
+//   slidesPerView: "1",
+//   coverflowEffect: {
+//       rotate: 0,
+//       stretch: 0,
+//       depth: 100,
+//       modifier: 4,
+//       slideShadows: false
+//   },
+//   keyboard: {
+//       enabled: true
+//   },
+//   mousewheel: {
+//       thresholdDelta: 70
+//   },
+//   initialSlide: 0,
+//   on: {
+//       click(event) {
+//           swiper.slideTo(this.clickedIndex);
+//       }
+//   },
+//   breakpoints: {
+//       640: {
+//           slidesPerView: 2
+//       }
+//   }
+// });
 
 /**
  * app.main
