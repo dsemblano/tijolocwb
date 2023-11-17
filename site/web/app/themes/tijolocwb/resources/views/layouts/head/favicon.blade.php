59442eb7d5c173348fc6a4d39490572cf57de3ca
Daniel Semblano <dsemblano@gmail.com>
2023-11-17 17:47:57 -0300

    New favicon, pink background, white text

--- site/web/app/themes/tijolocwb/resources/views/layouts/head/favicon.blade.php
+++ site/web/app/themes/tijolocwb/resources/views/layouts/head/favicon.blade.php
@@ -6,6 +6,6 @@
 <link rel="manifest" href="@asset('images/favicon/site.webmanifest')">
 <meta name="msapplication-config" content="@asset('images/favicon/browserconfig.xml')">
 <link rel="mask-icon" href="@asset('images/favicon/safari-pinned-tab_9f74bc90.svg" color="#e2aa9f')">
-<meta name="msapplication-TileColor" content="#da532c">
+<meta name="msapplication-TileColor" content="#e2aa9f">
 <meta name="msapplication-TileImage" content="/mstile-144x144.png">
-<meta name="theme-color" content="#ffffff">
+<meta name="theme-color" content="#E2AA9F">
