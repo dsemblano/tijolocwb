246ea51695cff437c9f897fd9d3c193df52449ab
Daniel Semblano <dsemblano@gmail.com>
2023-12-14 12:16:46 -0300

    Trellis upgrade + composer update plugins + WP update

--- site/web/app/themes/tijolocwb/resources/scripts/arrowtop.js
+++ site/web/app/themes/tijolocwb/resources/scripts/arrowtop.js
@@ -1,28 +1,20 @@
-jQuery(document).ready(function ($) {
-    // browser window scroll (in pixels) after which the "back to top" link is shown
-    // var offset = 300,
-    //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
-    //   offset_opacity = 1200,
-    //duration of the top scrolling animation (in ms)
-    //   scroll_top_duration = 700,
-    //grab the "back to top" link
-    //   $back_to_top = $('.cd-top');
-
+document.addEventListener("DOMContentLoaded", function() {
     //hide or show the "back to top" link
-    $(window).scroll(function () {
-        ($(this).scrollTop() > 300) ? $('.cd-top').addClass('cd-is-visible') : $('.cd-top').removeClass('cd-is-visible cd-fade-out');
-        if ($(this).scrollTop() > 1200) {
-            $('.cd-top').addClass('cd-fade-out');
+    window.onscroll = function() {
+        if (window.pageYOffset > 300) {
+            document.querySelector('.cd-top').classList.add('cd-is-visible');
+            document.querySelector('.cd-top').classList.remove('cd-fade-out');
+        } else {
+            document.querySelector('.cd-top').classList.remove('cd-is-visible');
         }
-    });
+        if (window.pageYOffset > 1200) {
+            document.querySelector('.cd-top').classList.add('cd-fade-out');
+        }
+    };
 
     //smooth scroll to top
-    $('.cd-top').on('click', function (event) {
+    document.querySelector('.cd-top').addEventListener('click', function(event) {
         event.preventDefault();
-        $('body,html').animate({
-            scrollTop: 0,
-        }, 700
-        );
+        window.scrollTo({top: 0, behavior: 'smooth'});
     });
-
 });
