f414cb66a66ad917eff4b8cf610016c4d2f300b6
Daniel Semblano <dsemblano@gmail.com>
2024-05-22 10:56:43 -0300

    Home image using picture and source media for mobile ands desktop fix

--- site/web/app/themes/tijolocwb/resources/views/partials/home/home1.blade.php
+++ site/web/app/themes/tijolocwb/resources/views/partials/home/home1.blade.php
@@ -41,9 +41,9 @@
       {{-- <div class="right mt-8 lg:mt-0 animate__animated animate__fadeInUp animate__slow lg:w-1/2"> --}}
         {{-- <img width="732" height="700" src="@asset('images/home-tijolo.webp')" alt="Tijolo CWB" /> --}}
         <picture class="right mt-8 lg:mt-0 animate__animated animate__fadeInUp animate__slow lg:w-1/2">
-          <source media="(max-width: 600px)" srcset="@asset('images/home-tijolo-mobile.webp')">
-          <source media="(min-width: 601px)" srcset="@asset('images/home-tijolo-desktop.webp')">
-          <img src="home-tijolo.webp" alt="Tijolo CWB restaurante foto interior">
+          <source media="(max-width: 600px)" srcset="@asset('images/home-tijolo-mobile.webp')" width="380" height="363">
+          <source media="(min-width: 601px)" srcset="@asset('images/home-tijolo-desktop.webp')" width="732" height="699">
+          <img src="home-tijolo-desktop.webp" alt="Tijolo CWB restaurante foto interior" width="732" height="699">
         </picture>
       {{-- </div> --}}
     </div>
