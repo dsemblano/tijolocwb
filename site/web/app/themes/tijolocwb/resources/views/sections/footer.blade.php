03c0bcc2bdcfecc61a165ea3a4f12ae69ddd4e83
Daniel Semblano <dsemblano@gmail.com>
2024-07-11 15:15:44 -0300

    footer padding top

--- site/web/app/themes/tijolocwb/resources/views/sections/footer.blade.php
+++ site/web/app/themes/tijolocwb/resources/views/sections/footer.blade.php
@@ -1,19 +1,19 @@
-<footer class="content-info bg-tijolo text-neutral-700 py-4">
+<footer class="content-info bg-tijolo text-neutral-700 py-4 pt-8">
   <div class="container">
     <div class="up-footer flex flex-col lg:flex-row justify-center items-center">
       <div class="left-footer">
-          <div class="logo-icons flex flex-col items-center justify-between gap-y-10">
-            <a class="brand" href="{{ home_url('/') }}">
-              <div class="text-neutral-700 logohome relative hover:scale-105 transition duration-300 ease-in-out">
-                <div class="text-8xl">Tijolo</div>
-                <div class="text-lg absolute top-[85%] right-0">comidas e vinhos</div>
-              </div>
-            </a>
-            <div id="social-icons" class="flex flex-row justify-between fill-neutral-700 gap-x-12">
-              @include ('partials.socialicons')
+        <div class="logo-icons flex flex-col items-center justify-between gap-y-10">
+          <a class="brand" href="{{ home_url('/') }}">
+            <div class="text-neutral-700 logohome relative hover:scale-105 transition duration-300 ease-in-out">
+              <div class="text-8xl">Tijolo</div>
+              <div class="text-lg absolute top-[85%] right-0">comidas e vinhos</div>
             </div>
+          </a>
+          <div id="social-icons" class="flex flex-row justify-between fill-neutral-700 gap-x-12">
+            @include ('partials.socialicons')
           </div>
-          
+        </div>
+
         {!! wp_nav_menu(['theme_location' => 'footer_navigation', 'menu_class' => 'flex footer-nav gap-4 w-full
         justify-center items-center lg:justify-start
         flex-row my-8 nav text-xl md:text-xl relative', 'echo' => false]) !!}
@@ -29,11 +29,12 @@
 
       <div class="left-footer">
         <section id="endereco" class="text-center">
-          <p>R. São Francisco, 179, lojas 2 e 3 – dentro da <a class="hover:underline" href="https://sfco179.com.br/" target="_blank">SFCO 179</a> <br> Centro Histórico, Curitiba - PR</p>
-            <p class="text-base">
-              <a title="Clique para a página dos horários" href="/horarios">> Horários</a>
-            </p>
-            {{-- <p>
+          <p>R. São Francisco, 179, lojas 2 e 3 – dentro da <a class="hover:underline" href="https://sfco179.com.br/"
+              target="_blank">SFCO 179</a> <br> Centro Histórico, Curitiba - PR</p>
+          <p class="text-base">
+            <a title="Clique para a página dos horários" href="/horarios">> Horários</a>
+          </p>
+          {{-- <p>
             Estacionamento em frente (ter- sáb)
           </p>
           <p>
