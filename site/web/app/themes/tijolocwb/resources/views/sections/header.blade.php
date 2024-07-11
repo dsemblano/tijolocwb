fb7c0ce0e4ba65a976ce5f933d296355c0f4aadf
Daniel Semblano <dsemblano@gmail.com>
2024-07-11 18:17:18 -0300

    padding on header

--- site/web/app/themes/tijolocwb/resources/views/sections/header.blade.php
+++ site/web/app/themes/tijolocwb/resources/views/sections/header.blade.php
@@ -1,5 +1,5 @@
 <header id="header" class="banner w-full z-50 bg-tijolo sticky top-0 left-0">
-  <nav class="nav-primary container py-1 nav-primary">
+  <nav class="nav-primary container py-2 nav-primary">
     <div class="flex flex-wrap lg:flex-nowrap justify-between items-center mx-auto">
       <a class="brand" href="{{ home_url('/') }}">
         @php
