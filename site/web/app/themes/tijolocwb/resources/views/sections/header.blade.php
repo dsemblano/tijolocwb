23a7aad32ff796abb44698746884b42a359f05c4
Daniel Semblano <dsemblano@gmail.com>
2024-07-11 23:10:12 -0300

    Shadows for header and sub-menu utilizing TailwindCSS

--- site/web/app/themes/tijolocwb/resources/views/sections/header.blade.php
+++ site/web/app/themes/tijolocwb/resources/views/sections/header.blade.php
@@ -1,4 +1,4 @@
-<header id="header" class="banner w-full z-50 bg-tijolo sticky top-0 left-0">
+<header id="header" class="banner w-full z-50 bg-tijolo sticky top-0 left-0 shadow">
   <nav class="nav-primary container py-2 nav-primary">
     <div class="flex flex-wrap lg:flex-nowrap justify-between items-center mx-auto">
       <a class="brand relative" href="{{ home_url('/') }}">
