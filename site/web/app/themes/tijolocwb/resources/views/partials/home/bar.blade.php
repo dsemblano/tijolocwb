cdd2c4de4da69e9cf9dcea03f387b5a9a485f78e
Daniel Semblano <dsemblano@gmail.com>
2024-10-22 17:19:47 -0300

    Homepage bar text html changed

--- site/web/app/themes/tijolocwb/resources/views/partials/home/bar.blade.php
+++ site/web/app/themes/tijolocwb/resources/views/partials/home/bar.blade.php
@@ -4,14 +4,17 @@
       <div class="left flex flex-col w-full lg:w-1/2 lourdinha-wrap items-center justify-around">
 
         <div class="text-center lg:text-left">
-          <p class="mb-6 lg:text-3xl">
-            Nosso cantinho “Lourdinha tô pronto” é uma lembrança da minha infância. A expressão era verbalizada pelo meu
-            avô, o Lutegardes, para a minha avó Lourdinha, nas intermináveis festas e eventos de família, quando ele já
-            estava além da felicidade, depois de degustar sorrateiramente seus drinks.
-          </p>
-          <p class="mb-6">
-            A hora da verdade: vovô se levantava calmamente da cadeira e se expressava: “Lourdinha, tô pronto”. Hora de
-            partir. Fim de festa. Amanhã tem mais. Nicolau</p>
+          <article class="text-lg lg:text-2xl">
+            <p class="mb-6">
+              Nosso cantinho “Lourdinha tô pronto” é uma lembrança da minha infância. A expressão era verbalizada pelo meu
+              avô, o Lutegardes, para a minha avó Lourdinha, nas intermináveis festas e eventos de família, quando ele já
+              estava além da felicidade, depois de degustar sorrateiramente seus drinks.
+            </p>
+            <p class="mb-6">
+              A hora da verdade: vovô se levantava calmamente da cadeira e se expressava: “Lourdinha, tô pronto”. Hora de
+              partir. Fim de festa. Amanhã tem mais. Nicolau
+            </p>
+          </article>
           </div>
 
         <picture id="lourdinha-logo" class="w-2/4 py-4">
