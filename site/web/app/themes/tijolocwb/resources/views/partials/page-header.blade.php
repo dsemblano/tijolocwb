8064b375c364b58f274b3891f15288e096461b68
Daniel Semblano <dsemblano@gmail.com>
2024-10-31 20:51:36 -0300

    Excerpt for Dia a Dia category

--- site/web/app/themes/tijolocwb/resources/views/partials/page-header.blade.php
+++ site/web/app/themes/tijolocwb/resources/views/partials/page-header.blade.php
@@ -1,3 +1,11 @@
 <div class="page-header mb-12">
   <h1 class="text-5xl text-center mt-8">{!! $title !!}</h1>
 </div>
+
+@php
+// Somente para a categoria Dia a Dia
+  $descricao_categoria = category_description();
+  if (!empty($descricao_categoria)) {
+      echo '<div id="categoria-descricao" class="text-center">' . $descricao_categoria . '</div>';
+  }
+@endphp 
