0aabfa606b8de843026c7276c533c42c18e7e225
Daniel Semblano <dsemblano@gmail.com>
2024-10-31 22:02:16 -0300

    back text for dia a dia posts

--- site/web/app/themes/tijolocwb/resources/views/partials/entry-meta.blade.php
+++ site/web/app/themes/tijolocwb/resources/views/partials/entry-meta.blade.php
@@ -2,6 +2,10 @@
   Postado em: {{ get_the_date() }}
 </time>
 
+<div>
+  <a class="no-underline" href="/nosso-dia-a-dia/">&larr; <span class="relative top-px">Voltar para Nosso dia a dia</span></a>
+</div>
+
 {{-- <p>
   <span>{{ __('By', 'sage') }}</span>
   <a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" class="p-author h-card">
