5fabc43549d9e6cc4139ae6b0453ab070cf4fd5c
Daniel Semblano <dsemblano@gmail.com>
2024-11-05 00:00:52 -0300

    Title Como Chegar for maps at home fix with taiwindcss class

--- site/web/app/themes/tijolocwb/resources/views/partials/home/home3.blade.php
+++ site/web/app/themes/tijolocwb/resources/views/partials/home/home3.blade.php
@@ -1,6 +1,6 @@
 <section id="tela3" class="home-section bg-tijoloyellow flex py-10">
   <div class="container flex flex-col justify-center items-center">
-    <h2>Como chegar</h2>
+    <h2 class="mb-6">Como chegar</h2>
     @include('partials/snippets.gmaps')
     {{-- @include('partials/snippets/reservation') --}}
     {{-- <h2 class="text-lg xl:text-2xl text-gray-800 top-10">Rua São Francisco 179 loja 3,<br />Curitiba PR CEP 80020-190</h2>
