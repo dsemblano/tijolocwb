c9b067445da1f9dd1dfca33976f574422319ab7f
Daniel Semblano <dsemblano@gmail.com>
2024-11-13 20:59:36 -0300

    Fixed image description for Bar photo

--- site/web/app/themes/tijolocwb/resources/views/partials/home/bar.blade.php
+++ site/web/app/themes/tijolocwb/resources/views/partials/home/bar.blade.php
@@ -27,7 +27,7 @@
       <picture class="right mt-8 lg:mt-0 lg:w-1/2">
         <source media="(max-width: 600px)" srcset="@asset('images/lourdinha-bar-taca-mobile.webp')" width="380" height="380">
         <source media="(min-width: 601px)" srcset="@asset('images/lourdinha-bar-taca-desktop.webp')" width="604" height="604">
-        <img src="@asset('images/lourdinha-bar-taca-desktop.webp')" alt="Tijolo CWB restaurante foto interior" width="604" height="604">
+        <img src="@asset('images/lourdinha-bar-taca-desktop.webp')" alt="Foto Bar Lourdinha tô Pronto, dentro do Tijolo Restaurante" width="604" height="604">
       </picture>
 
     </div>
