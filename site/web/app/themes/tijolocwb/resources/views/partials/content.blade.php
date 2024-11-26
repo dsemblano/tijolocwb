e52c6a454aa3e38f804cc2beb84a302f82f3c969
Daniel Semblano <dsemblano@gmail.com>
2024-11-26 17:56:42 -0300

    New grid layout, typography fixes

--- site/web/app/themes/tijolocwb/resources/views/partials/content.blade.php
+++ site/web/app/themes/tijolocwb/resources/views/partials/content.blade.php
@@ -4,8 +4,8 @@
       {{ the_post_thumbnail('medium_large', array( 'class' => 'w-full rounded' ) ) }}
     </figure>
   </a>
-  <header class="nossodia-header relative bottom-20 w-4/5 m-auto p-4 rounded-t-md bg-white">
-      <h2 class="text-center mt-0">
+  <header class="nossodia-header relative bottom-20 w-10/12 m-auto p-4 rounded-t-md bg-white">
+      <h2 class="text-center mt-0 text-lg md:text-2xl">
         <a class="postslinks" href="{{ get_permalink() }}">
           {!! $title !!}
         </a>
