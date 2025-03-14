3f9772c6eeb154c2ce83227d33c09ef6afdff635
Daniel Semblano <dsemblano@gmail.com>
2025-03-14 11:07:07 -0300

    composer update plugins, fix on header single page

---
+++ site/web/app/themes/tijolocwb/resources/views/partials/home/carousel.blade.php
@@ -0,0 +1,1 @@
+{!! do_shortcode( '[carousel_slide id="3717"]' ) !!}
--- site/web/app/themes/tijolocwb/resources/views/partials/page-header.blade.php
+++ site/web/app/themes/tijolocwb/resources/views/partials/page-header.blade.php
@@ -1,5 +1,5 @@
-<div class="page-header">
-  <h1 class="text-5xl text-center mt-8 font-bold animate__animated animate__fadeInDown">{!! $title !!}</h1>
+<div class="page-header not-prose mt-12 mb-4 md:mt-24 text-center text-6xl lg:text-8xl">
+  <h1 class="text-center mt-8 font-bold animate__animated animate__fadeInDown">{!! $title !!}</h1>
 </div>
 
 @php
