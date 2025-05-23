aa14f4f294abb23cd38d1a5ab6008707ad8185b2
Daniel Semblano <dsemblano@gmail.com>
2025-05-23 17:01:43 -0300

    New logo with png, deleted old font

--- site/web/app/themes/tijolocwb/resources/views/layouts/head/preload.blade.php
+++ site/web/app/themes/tijolocwb/resources/views/layouts/head/preload.blade.php
@@ -1,4 +1,2 @@
 <link rel="preload" fetchpriority="high" as="image" href="@asset('images/TijoloEntrada.webp')" type="image/webp">
-<link rel="preload" href="@asset('fonts/Acumin/acumin_pro_extracondensed-webfont.woff2')" as="font" type="font/woff2"
-    crossorigin>
 <link rel="preload" href="@asset('fonts/Cuprum/Cuprum-Regular-webfont.woff')" as="font" type="font/woff" crossorigin>
--- site/web/app/themes/tijolocwb/resources/views/partials/logo.blade.php
+++ site/web/app/themes/tijolocwb/resources/views/partials/logo.blade.php
@@ -1,6 +1,5 @@
 <a class="brand relative" href="{{ home_url('/') }}">
     @php
-    // $current = (! is_front_page() ||! is_woocommerce() || ! is_shop() ? "div" : 'h1');
     $current = (! is_front_page() ? "div" : 'h1');
 
     if (is_category()) {
@@ -15,21 +14,9 @@
         }
     }
 
-    // $current_page = (! is_category() ? get_the_title() : single_cat_title('', false) );
-    
-    // $current_page = (! is_category() || ! is_page() ? get_the_category(get_the_ID())[0]->name : get_the_title('', false) );
-    // $current_page = !is_category() 
-    // ? htmlspecialchars_decode(get_the_title(), ENT_QUOTES) 
-    // : htmlspecialchars_decode(single_cat_title('', false), ENT_QUOTES);
     @endphp
-    <{{$current}} id="logo"
-      class="text-white logohome relative bottom-4 hover:scale-105 transition duration-300 ease-in-out">
-      {{-- <span id="logoname" class="text-8xl relative">Tijolo</span> --}}
-      <span id="logoname">Tijolo</span>
-      <span id="logosurname" class="text-lg absolute top-[85%] right-0">Cozinha Contemporânea</span>
+    <{{$current}}>
+      <img id="logoname" class="hover:scale-105 transition duration-300 ease-in-out" src="@asset('images/logo/tijolologo.png')" alt="Tijolo logo" width="134" height="138">
 
-      @if (! is_front_page())
-      <span id="logosurnamepage" class="text-lg absolute top-[80%] left-0 hidden"><?php echo esc_html($current_page); ?></span>
-      @endif
     </{{$current}}>
   </a>
