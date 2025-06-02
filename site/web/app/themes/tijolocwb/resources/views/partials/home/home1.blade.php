75186798661b8596843844d67ec5eaca6446ef84
Daniel Semblano <dsemblano@gmail.com>
2025-06-02 16:11:01 -0300

    Default font size text-lg for all headings

--- site/web/app/themes/tijolocwb/resources/views/partials/home/home1.blade.php
+++ site/web/app/themes/tijolocwb/resources/views/partials/home/home1.blade.php
@@ -3,7 +3,7 @@
     <div class="flex flex-col lg:flex-row gap-x-10 text-white">
       <div class="left flex flex-col w-full lg:w-1/3">
 
-        <article class="text-center lg:text-left text-lg">
+        <article class="text-center lg:text-left">
           <p class="mb-6 animate__animated animate__fadeInLeftBig animate__fast">
             O Tijolo Restaurante é um projeto de comida descomplicada, no centro histórico de Curitiba. Nosso conceito é baseado no slow food, na experiência, na produção com cuidado e atenção plena, sem a pressa do fast food e do cotidiano.
           </p>
--- site/web/app/themes/tijolocwb/resources/views/partials/page-header.blade.php
+++ site/web/app/themes/tijolocwb/resources/views/partials/page-header.blade.php
@@ -1,5 +1,5 @@
-<div class="page-header not-prose mt-12 mb-4 md:mt-24 text-center text-6xl lg:text-8xl">
-  <h1 class="text-center mt-8 font-bold animate__animated animate__fadeInDown">{!! $title !!}</h1>
+<div class="page-header not-prose mt-14 mb-12  text-center text-6xl">
+  <h1 class="text-center font-bold animate__animated animate__fadeInDown">{!! $title !!}</h1>
 </div>
 
 @php
