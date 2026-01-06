dd7010fa50b956b5358a910f8bdc7c8ba4af467b
Daniel Semblano <dsemblano@gmail.com>
2026-01-06 16:23:58 -0300

    Bar section redone images

--- site/web/app/themes/tijolocwb/resources/views/partials/home/bar.blade.php
+++ site/web/app/themes/tijolocwb/resources/views/partials/home/bar.blade.php
@@ -1,35 +1,34 @@
-<section id="bar" class="home-section flex justify-start items-start bg-tijoloyellow py-6">
-  <div class="container flex justify-start">
-    <div class="flex flex-col lg:flex-row gap-x-10 text-tijoloyellowp w-full">
-      <div class="left flex flex-col w-full lg:w-1/2 lourdinha-wrap items-center justify-around">
+<section id="bar" class="home-section bg-tijoloyellow py-6">
+    <div class="flex flex-col lg:flex-row gap-x-10 text-tijoloyellowp w-full container">
+        <div class="left flex flex-col w-full lg:w-1/2 lourdinha-wrap items-center justify-around">
 
-        <div class="text-left">
-          <article class="text-lg">
-            <p class="mb-6">
-              Nosso cantinho “Lourdinha tô pronto” é uma lembrança da minha infância. A expressão era verbalizada pelo meu
-              avô, o Lutegardes, para a minha avó Lourdinha, nas intermináveis festas e eventos de família, quando ele já
-              estava além da felicidade, depois de degustar sorrateiramente seus drinks.
-            </p>
-            <p class="mb-6">
-              A hora da verdade: vovô se levantava calmamente da cadeira e se expressava: “Lourdinha, tô pronto”. Hora de
-              partir. Fim de festa. Amanhã tem mais. Nicolau
-            </p>
-          </article>
-          </div>
+            <div class="text-left">
+                <article class="text-lg">
+                    <p class="mb-6">
+                        Nosso cantinho “Lourdinha tô pronto” é uma lembrança da minha infância. A expressão era
+                        verbalizada pelo meu
+                        avô, o Lutegardes, para a minha avó Lourdinha, nas intermináveis festas e eventos de
+                        família, quando ele já
+                        estava além da felicidade, depois de degustar sorrateiramente seus drinks.
+                    </p>
+                    <p class="mb-6">
+                        A hora da verdade: vovô se levantava calmamente da cadeira e se expressava: “Lourdinha, tô
+                        pronto”. Hora de
+                        partir. Fim de festa. Amanhã tem mais. Nicolau
+                    </p>
+                </article>
+            </div>
 
-        <picture id="lourdinha-logo" class="w-2/4 py-4">
-          <source media="(min-width: 600px)" srcset="@asset('images/Lourdinha.webp')" width="509" height="300">
-          <img src="@asset('images/Lourdinha.webp')" alt="Lourdinha logo" width="509" height="300">
+            <picture id="lourdinha-logo" class="w-2/4 py-4">
+                <img src="@asset('images/barlogo.webp')" alt="Lourdinha logo" width="509" height="300">
+            </picture>
+
+        </div>
+
+        <picture class="right mt-8 lg:mt-0 lg:w-1/2">
+            <img src="@asset('images/barlogo2.webp')" alt="Foto Bar Lourdinha tô Pronto, dentro do Tijolo Restaurante" width="604"
+                height="604">
         </picture>
 
-      </div>
-
-      <picture class="right mt-8 lg:mt-0 lg:w-1/2">
-        <source media="(max-width: 600px)" srcset="@asset('images/lourdinha-mobile.webp')" width="380" height="380">
-        <source media="(min-width: 601px)" srcset="@asset('images/lourdinha-desktop.webp')" width="604" height="604">
-        <img src="@asset('images/lourdinha-desktop.webp')" alt="Foto Bar Lourdinha tô Pronto, dentro do Tijolo Restaurante" width="604" height="604">
-      </picture>
-
     </div>
-  </div>
 </section>
