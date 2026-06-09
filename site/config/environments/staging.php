2e232558f7fa9fbf76f6340d26a5c2e5e56fa4de
Daniel Semblano <dsemblano@gmail.com>
2026-06-09 14:56:50 -0300

    debug error on staging

--- site/config/environments/staging.php
+++ site/config/environments/staging.php
@@ -14,4 +14,6 @@ use Roots\WPConfig\Config;
  * Example: `Config::define('DISALLOW_FILE_MODS', false);`
  */
 
+Config::define('WP_DEBUG', true);
+Config::define('WP_DEBUG_DISPLAY', true);
 Config::define('DISALLOW_INDEXING', true);
